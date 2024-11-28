e7fbb1136a19615d841912ebab1b4b156e458aa6
Adiel Dantas <adieldantasn@gmail.com>
2024-11-28 10:44:03 -0300

    GET client paginado

--- src/main/java/com/devsuperior/javaspringprofessional/desafio03/services/ClientService.java
+++ src/main/java/com/devsuperior/javaspringprofessional/desafio03/services/ClientService.java
@@ -5,6 +5,8 @@ import com.devsuperior.javaspringprofessional.desafio03.entities.Client;
 import com.devsuperior.javaspringprofessional.desafio03.repositories.ClientRepository;
 import com.devsuperior.javaspringprofessional.desafio03.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +21,11 @@ public class ClientService {
         Client client = repository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Recurso não localizado"));
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
